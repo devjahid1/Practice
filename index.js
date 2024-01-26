f32ac935590333ed77d1122078016120596810bb
jahid <developerjahid1@gmail.com>
2024-01-27 04:04:11 +0600

    my commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var orange, apple, sum;
-orange = (parseInt('230'));
-apple = (parseInt('210'));
-sum = (orange+apple);
-console.log(sum);
+var math, bio, phy, che, ban, sum;
+math = (parseFloat('75.5'));
+bio = (parseInt('65'));
+phy = parseInt('80');
+che = parseFloat('35.5');
+ban = parseFloat('99.5');
+sum = math+bio+phy+che+ban;
+console.log(sum.toFixed(2));
