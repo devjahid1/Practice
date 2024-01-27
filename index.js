b125af7c8a99b8d29572f23842d118f9a95ae2cd
jahid <developerjahid1@gmail.com>
2024-01-28 02:18:58 +0600

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-var math, bio, phy, che, ban, sum;
-math = (parseFloat('75.5'));
-bio = (parseInt('65'));
-phy = parseInt('80');
-che = parseFloat('35.5');
-ban = parseFloat('99.5');
-sum = math+bio+phy+che+ban;
-console.log(sum.toFixed(2));
+var bmi,  weight=69,  height= 3;
+bmi = weight / (height) ^ 2;
+if(bmi < 18.5){
+    console.log("Under Weight");
+}
+else if(bmi >= 18.5 && bmi <= 24.9){
+    console.log("Normal");
+}
+else if(bmi >= 25 && bmi <= 29.9){
+    console.log("Over Weight");
+}
+else{
+    console.log(" Yoou are dad");
+}
+
