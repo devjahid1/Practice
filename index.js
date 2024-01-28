e7313f7696b4f44b028c4c6510000528b381d1d4
jahid <developerjahid1@gmail.com>
2024-01-29 02:04:25 +0600

    s

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let number = [1, 2, 3, 4, 5];
-console.log(number[2]);
-let abe = 100;
-number[2]=100;
-console.log(number)
+console.log(number);
+number.push(6);
+console.log(number);
+number.push(7);
+console.log(number);
