926b28fe209deb2304e025ef75e1e3102d4ef58a
jahid <developerjahid1@gmail.com>
2024-02-02 00:51:26 +0600

    m

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-for(let n = 10; n >= 0; n--){
-    console.log(n);
+const sub = `Chemistry`;
+const book = `chemistry`;
+console.log(sub.toLocaleLowerCase());
+console.log(book.toLocaleLowerCase());
+if(sub.toLocaleLowerCase() == book.toLocaleLowerCase()){
+    console.log("Same");
+}
+else{
+    console.log("not same");
 }
