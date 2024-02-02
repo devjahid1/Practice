6f46e3ff7309ad0a4cc102625040f20e8c8d5bf5
jahid <developerjahid1@gmail.com>
2024-02-03 03:44:24 +0600

    t

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-function myFun(){
-    console.log(`Turn On The Fan`);
-    console.log(`Walk towards the switch`);
-    console.log(`Click the switch to off the fan`);
-}
-myFun();
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+console.log(text)
