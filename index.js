e589eeffa99853c50800076c199320c50a5e6329
jahid <developerjahid1@gmail.com>
2024-02-05 02:44:16 +0600

    m

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const fruits = ["Banana", "Orange", "Apple", "Mango"];
-
-console.log(text)
+function diff(num1, num2){
+const sum = num1 - num2;
+console.log(sum);
+}
+const f = 10;
+const y = 5;
+diff(f, y);
