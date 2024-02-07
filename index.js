fef6faf045f6639f40f2eeeca80aced79c3b1813
jahid <developerjahid1@gmail.com>
2024-02-07 06:35:52 +0600

    d

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-function leapYear(year){
-    if(year % 4 === 0){
-        return true;
-    }
-    else if(year % 100 !== 0 && year %400 === 0){
-        return true;
-    }
-    else{
-        return false
+function oddAvg(numbers){
+const odd=[];
+for(const num of numbers){
+    if(num % 2 ===1){
+        odd.push(num);
     }
 }
-const result=leapYear(2024);
-console.log(result);
+console.log(odd);
+let sum = 0;
+for(const od of odd){
+    sum+= od;
+}
+const count = odd.length;
+console.log(sum, count);
+const average=
+ sum / count;
+return average;
+ }
+const numbers=[42, 13, 58, 65, 81, 7];
+const avg=oddAvg(numbers)
+console.log(`Average Number Is:`, avg);
