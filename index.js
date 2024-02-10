5b205d39f5a880d31b09aeb8234ff08b99cb04b8
jahid <developerjahid1@gmail.com>
2024-02-10 23:48:37 +0600

    l

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-function leapYear(year){
-    if(year % 4 === 0){
-        return true;
+const products=[
+    {id: 1, name: 'walton phone', price: '14000'},
+    {id: 2, name: 'Samsung phone', Price: '35000'},
+    {id: 3, name: 'Pixel Phone', price: '63000'},
+    {id: 4, name: 'iphone Phone Laptop', price: '195000'},
+    {id: 5, name: 'xiomi phone', price: '22000'},
+    {id: 6, name: 'intel phone', price: '12000'},
+    {id: 7, name: 'oppo phone', price: '19000'},
+    {id: 8, name: 'Vivo phone', price: '24000'},
+]
+
+function matchProducts(products, search){
+    const matched = [];
+    for(const product of products){
+        if(product.name.includes(search)){
+            matched.push(product);
+        }
     }
-    else if(year % 100 !== 0 && year %400 === 0){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return matched;
 }
-console.log(leapYear(2024));
+const result = matchProducts(products, 'phone');
+console.log(result);
